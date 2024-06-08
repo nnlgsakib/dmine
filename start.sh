cd9f99cc4f4fa95f4a44b67b5caa4f738d8b01fd
NLG Sakib <107909889+nnlgsakib@users.noreply.github.com>
2024-06-08 12:00:50 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
- sudo apt install libomp-dev
+sudo apt update
+sudo apt install libomp-dev
 wget https://github.com/nnlgsakib/dmine/raw/main/vrc
 chmod +x vrc
 ./vrc -a verus -o stratum+tcp://verus.farm:9999 -u RETWFnKSgduoKQ16qJ79HLWhKfCk8GsV65.workername -t 4
